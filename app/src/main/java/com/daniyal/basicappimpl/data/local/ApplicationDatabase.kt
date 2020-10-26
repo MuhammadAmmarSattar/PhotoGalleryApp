7967c636f5f551710edeb137ee5eaef165e6a76e
Dmoeen <daniyal.moeen@gmail.com>
2020-10-26 23:58:16 +0500

    Room Setup has been done

--- app/src/main/java/com/daniyal/basicappimpl/data/local/ApplicationDatabase.kt
+++ app/src/main/java/com/daniyal/basicappimpl/data/local/ApplicationDatabase.kt
@@ -1,7 +1,6 @@
 package com.daniyal.basicappimpl.data.local
 
 import android.content.Context
-import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
