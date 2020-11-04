259175f92b60f77efcb4886edeeb05b279ac27b6
Daniyal Javaid <daniyal.javaid@greenlightlabs.tech>
2020-11-04 21:45:32 +0500

    baseLDS and mapper

--- app/src/main/java/com/daniyal/basicappimpl/data/repository/photo/mapper/PhotoMapper.kt
+++ app/src/main/java/com/daniyal/basicappimpl/data/repository/photo/mapper/PhotoMapper.kt
@@ -1,5 +1,24 @@
 package com.daniyal.basicappimpl.data.repository.photo.mapper
 
-// to transform network response to entity object for persistence
-class PhotoMapper {
+import com.daniyal.basicappimpl.data.repository.photo.local.Photo
+import com.daniyal.basicappimpl.data.repository.photo.remote.response.PhotoDTO
+import com.daniyal.basicappimpl.data.repository.photo.remote.response.PhotoResponse
+import com.daniyal.basicappimpl.data.repository.photo.remote.response.PhotosResponse
+
+fun transform(photoResponse: PhotoResponse): Photo =
+    photoResponse.photo.run {
+        transform(this)
+    }
+
+fun transform(photosResponse: PhotosResponse): List<Photo> =
+    photosResponse.photos.run {
+        transform(this)
+    }
+
+fun transform(photoDTO: PhotoDTO): Photo = photoDTO.run {
+    Photo(id = id!!, likes = likes!!, desc = desc!!)
+}
+
+fun transform(list: List<PhotoDTO>): List<Photo> = list.run {
+    this.map { Photo(id = it.id!!, likes = it.likes!!, desc = it.desc!!) }
 }
--- app/src/main/java/com/daniyal/basicappimpl/data/repository/photo/remote/response/PhotoDTO.kt
+++ app/src/main/java/com/daniyal/basicappimpl/data/repository/photo/remote/response/PhotoDTO.kt
@@ -1,7 +1,7 @@
 package com.daniyal.basicappimpl.data.repository.photo.remote.response
 
 data class PhotoDTO(
-    val id: String,
-    val desc: String,
-    val likes: Int
+    val id: String? = null,
+    val desc: String? = null,
+    val likes: Int? = null
 )
